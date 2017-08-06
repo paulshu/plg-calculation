9588dd112d922b88e5d1abc021bdfc9349a07a8f
paulshu <paulshu18@gmail.com>
2017-08-06 10:51:18 +0800

    inplmement exprot csv

--- app/controllers/compression_springs_controller.rb
+++ app/controllers/compression_springs_controller.rb
@@ -1,3 +1,4 @@
+require 'csv'
 class CompressionSpringsController < ApplicationController
   before_action :authenticate_user!
 
@@ -7,6 +8,22 @@ class CompressionSpringsController < ApplicationController
 
   def show
     @compression_spring = CompressionSpring.find(params[:id])
+
+    respond_to do |format| # 以下代码用于导出CSV文件
+      format.html
+      format.csv {
+        #@compression_springs = @compression_spring.reorder("id ASC") 因为这里只有一个产品
+        csv_string = CSV.generate do |csv|
+          csv << ["开门弹簧长度L1","关门弹簧长度L2","开门最小负荷","关门最大负荷","开门弹簧力F1","关门弹簧力F2","弹簧有效圈数",
+            "弹簧总圈数","弹簧线径","弹簧内径","弹簧自由长度","弹簧螺旋升角"]
+          cs = @compression_spring
+            csv << [cs.od_length, cs.cd_length, cs.min_force, cs.max_force, cs.od_force.round(3), cs.cd_force.round(3), cs.active_coil_num,
+              cs.total_num, cs.wire_diameter, cs.inside_diameter, cs.free_lengh, cs.spring_helix_angle.round(3)]
+
+        end
+        send_data csv_string, :filename => "#{@compression_spring.product_name}-压缩弹簧设计参数-#{@compression_spring.product_number}.csv"
+      }
+    end
   end
 
   def new
@@ -77,7 +94,7 @@ class CompressionSpringsController < ApplicationController
   def step3_update
     @compression_spring = CompressionSpring.find(params[:id])
     @compression_spring.current_step = 3
-    
+
     if @compression_spring.update(compression_spring_params)
       @compression_spring.total_num?
       @compression_spring.od_force?
