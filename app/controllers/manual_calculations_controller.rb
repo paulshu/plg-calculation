e057cdd53490e99700e7884be5a7593dbf501277
paulshu <paulshu18@gmail.com>
2017-08-12 14:04:52 +0800

    implement manual_calculation

--- app/controllers/manual_calculations_controller.rb
+++ app/controllers/manual_calculations_controller.rb
@@ -40,13 +40,15 @@ class ManualCalculationsController < ApplicationController
   def destroy
     @manual_calculation = ManualCalculation.find(params[:id])
     @manual_calculation.destroy
-
+    flash[:alert] = "#{@manual_calculation.product_name}删除成功"
     redirect_to manual_calculations_path
   end
 
   private
 
   def manual_calculation_params
-    params.require(:manual_calculation).permit()
+    params.require(:manual_calculation).permit(:product_name,:product_number, :hinge_x, :hinge_y, :hinge_z, :centre_gravity_x, :centre_gravity_y, :centre_gravity_z,
+            :door_weight, :body_a_x, :body_a_y, :body_a_z, :gate_b_x, :gate_b_y, :gate_b_z, :open_handle_x, :open_handle_y, :open_handle_z, :close_handle_x, :close_handle_y,
+            :close_handle_z, :open_angle, :open_time, :close_time, :open_dynamic_friction, :close_dynamic_friction, :open_static_friction, :close_static_friction )
   end
 end
--- app/models/manual_calculation.rb
+++ app/models/manual_calculation.rb
@@ -1,2 +1,6 @@
 class ManualCalculation < ApplicationRecord
+
+  validates :product_name, presence:  { message: "请填写产品或项目名称" }
+  validates :product_number,  presence:  { message: "请填写产品编号" },
+                              uniqueness: { message: "产品编号重复了" }
 end
